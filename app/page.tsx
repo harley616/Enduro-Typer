ad776d57f6ff88ec6c9c56b8e0cf787e305adb0e
Harley Ewert <harleyewert@Harleys-MacBook-Pro.local>
2023-10-08 09:50:51 -0600

    added login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,74 +1,15 @@
-"use client";
-import { use, useState } from "react"
+import Link from "next/link"
 
-let test = ["This is the first test string.", "This is the second string.", "Now you are getting after it, here is the third string."]
-export default function Home() {
-  const [wpmList, setWpmList] = useState<Array<number>>([]);
-  const [iter, setIter] = useState(0);
-  const [startTime, setStartTime] = useState(0);
-  const [testString, setTest] = useState<string>(test[iter]);
-  const [okay, setOkay] = useState<boolean>(true);
-  const [done, setDone] = useState(false);
-  const [wpm, setWpm] = useState(0)
-  function updateWpm(){
-    setWpm(
-      Math.floor((testString.split(' ').length/(Date.now() - startTime)*1000)*60)
-    )
-  }
-  function avg(lst: Array<number>){
-    let sum = 0
-  
-    lst.forEach(el => {
-      sum = sum + el
-    });
-    return sum/lst.length
-  }
-  return (
+export default function Home(){
+  return(
     <div>
-      <h1>Welcome to Typer</h1>
-      {okay ? 
-      <>
-      {!done ? 
-      <>
-      <div className="relative">
-
-      <textarea className="w-64 h-24 bg-sky-100 text-black p-1" onChange={(e) => {
-        if(e.target.value.charAt(e.target.value.length - 1) !== testString[e.target.value.length - 1]){
-          setOkay(false);
-          setIter(0);
-        }else{
-          if(e.target.value.charAt(e.target.value.length - 1) === testString[0]){
-            setStartTime(Date.now());
-          }
-          if(e.target.value.charAt(e.target.value.length - 1) === testString[testString.length -1]){
-            setDone(true);
-            setIter(iter + 1)
-            updateWpm();
-          }
-        }
-        }}>
-      </textarea>
-      <div className="w-64 h-24 bg-none text-black opacity-25 absolute top-0 left-0 pointer-events-none p-1">
-        {testString}
+      <h1>Welcome to Enduro Typer!</h1>
+      <div>
+        <h2>You can login if you want to track your scores</h2>
+        <Link href="login">Login</Link>
       </div>
-      </div>
-     
-      </>
-      :
-      <>
-      <h1> Wpm: {wpm}</h1>
-      <div onClick={() => {setOkay(true); setDone(false), setTest(test[iter]), setWpmList([...wpmList, wpm])}}>Next</div>
-      </>
-      }
-      </> : 
-      <>
-      <div> Failed</div>
-      <div onClick={() => {setOkay(true), setDone(false),setTest(test[iter])}}>Try again</div>
-      <div>You had an avg apm of: {avg(wpmList)}</div>
-      </>
-
-      }
-      
+      <Link href="game">Play</Link>
+        
     </div>
   )
 }
